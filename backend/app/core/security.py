5bba776e55b99220c035f9152ae85a170debfedc
McCree0919 <caiyin@kean.edu>
2026-04-14 21:34:59 -0700

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime, timedelta, timezone
 from typing import Literal
 
-from jose import JWTError, jwt
+from jose import jwt
 from passlib.context import CryptContext
 
 from .settings import get_settings
